f071d485495ae32594452c1bf502e9330b1a598e
Ciaran Finnegan <ciaran@feefinnegan.com>
2023-02-23 18:26:02 +0000

    Shell rebuild

--- Source Code/Shells/bookcopy.sh
+++ Source Code/Shells/bookcopy.sh
@@ -13,7 +13,12 @@ ls ~/shared -l
 echo " "
 echo "Copy Gutenberg book file from Host..."
 echo " "
-sudo cp ~/shared/Il_Diavolo.txt Il_Diavolo
+sudo cp ~/shared/Il_Diavolo.txt Il_Diavolo_Italian
+sudo cp ~/shared/Die_Postgeheimnisse.txt Die_Postgeheimnisse_German
+sudo cp ~/shared/DivinaCommedia.txt DivinaCommedia_Italian
+sudo cp ~/shared/Don_Quijote.txt Don_Quijote_Spanish
+sudo cp ~/shared/Goethe_Biographie.txt Goethe_Biographie_German
+sudo cp ~/shared/La_isla_del_tesoro.txt La_isla_del_tesoro_Spanish
 echo " "
 
 echo " "
@@ -24,8 +29,12 @@ ls -l
 echo " "
 echo "Set up file permission on VM..."
 echo " "
-sudo chown soc:soc Il_Diavolo
-
+sudo chown soc:soc Il_Diavolo_Italian
+sudo chown soc:soc Die_Postgeheimnisse_German
+sudo chown soc:soc DivinaCommedia_Italian
+sudo chown soc:soc Don_Quijote_Spanish
+sudo chown soc:soc Goethe_Biographie_German
+sudo chown soc:soc La_isla_del_tesoro_Spanish
 
 echo " "
 echo " "
--- Source Code/Shells/loadbooks2HDFS.sh
+++ Source Code/Shells/loadbooks2HDFS.sh
@@ -10,4 +10,10 @@ hadoop fs -mkdir gutenbergBooks
 echo " "
 echo " "
 echo "Load book data into HDFS..."
-hadoop fs -put Il_Diavolo gutenbergBooks
+hadoop fs -put Il_Diavolo_Italian gutenbergBooks
+hadoop fs -put Die_Postgeheimnisse_German gutenbergBooks
+hadoop fs -put DivinaCommedia_Italian gutenbergBooks
+hadoop fs -put Don_Quijote_Spanish gutenbergBooks
+hadoop fs -put Goethe_Biographie_German gutenbergBooks
+hadoop fs -put La_isla_del_tesoro_Spanish gutenbergBooks
+
